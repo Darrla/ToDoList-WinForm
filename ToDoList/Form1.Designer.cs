ed46bfc193aefb8bd1801732564327b18d057002
Darrla <udasa8871@gmail.com>
2024-12-22 15:04:29 +0300

    Добавьте файлы проекта.

---
+++ ToDoList/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace ToDoList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.taskTextBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.taskListBox = new System.Windows.Forms.ListBox();
+            this.removeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Location = new System.Drawing.Point(341, 37);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(81, 16);
+            this.titleLabel.TabIndex = 0;
+            this.titleLabel.Text = "Список дел";
+            this.titleLabel.Click += new System.EventHandler(this.titleLabel_Click);
+            // 
+            // taskTextBox
+            // 
+            this.taskTextBox.Location = new System.Drawing.Point(276, 262);
+            this.taskTextBox.Name = "taskTextBox";
+            this.taskTextBox.Size = new System.Drawing.Size(216, 22);
+            this.taskTextBox.TabIndex = 1;
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(276, 301);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(84, 23);
+            this.addButton.TabIndex = 2;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // taskListBox
+            // 
+            this.taskListBox.FormattingEnabled = true;
+            this.taskListBox.ItemHeight = 16;
+            this.taskListBox.Location = new System.Drawing.Point(120, 56);
+            this.taskListBox.Name = "taskListBox";
+            this.taskListBox.Size = new System.Drawing.Size(537, 180);
+            this.taskListBox.TabIndex = 3;
+            // 
+            // removeButton
+            // 
+            this.removeButton.Location = new System.Drawing.Point(398, 301);
+            this.removeButton.Name = "removeButton";
+            this.removeButton.Size = new System.Drawing.Size(84, 23);
+            this.removeButton.TabIndex = 4;
+            this.removeButton.Text = "Удалить";
+            this.removeButton.UseVisualStyleBackColor = true;
+            this.removeButton.Click += new System.EventHandler(this.removeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.removeButton);
+            this.Controls.Add(this.taskListBox);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.taskTextBox);
+            this.Controls.Add(this.titleLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox taskTextBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.ListBox taskListBox;
+        private System.Windows.Forms.Button removeButton;
+    }
+}
+
---
+++ ToDoList/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToDoList
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            string task = taskTextBox.Text;
+            if (!string.IsNullOrWhiteSpace(task))
+            {
+                taskListBox.Items.Add(task);
+                taskTextBox.Clear(); 
+            }
+            else
+            {
+                MessageBox.Show("Введите задачу.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void removeButton_Click(object sender, EventArgs e)
+        {
+            if (taskListBox.SelectedIndex != -1)
+            {
+                taskListBox.Items.RemoveAt(taskListBox.SelectedIndex); 
+            }
+            else
+            {
+                MessageBox.Show("Выберите задачу для удаления.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        private void titleLabel_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
